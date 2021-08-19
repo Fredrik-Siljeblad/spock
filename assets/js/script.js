e34551288e7fa4c07738d0364af907ecec148aa9
Fredrik-Siljeblad <fredrik.siljeblad@gmail.com>
2021-08-19 14:26:51 +0200

    basic layout done

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Do not run the game until the DOM is loaded
+
+// Create buttons object, add listeners to all buttons
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+}
+
+// Add listener to the "best of".
